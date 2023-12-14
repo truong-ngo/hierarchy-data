932495ccc184db03be817d1092e9f42edbf33d42
truong-ngo <truong.ngo1993@gmail.com>
2023-12-14 16:21:25 +0700

    add

--- read-write-table/src/main/java/com/example/readwritetable/repository/ReadNodeRepo.java
+++ read-write-table/src/main/java/com/example/readwritetable/repository/ReadNodeRepo.java
@@ -8,10 +8,10 @@ import java.util.List;
 
 public interface ReadNodeRepo extends JpaRepository<ReadNode, Long> {
     @Query(value = "select children.* from nested_set as node, nested_set as children\n" +
-                   "where node.lft < children.lft and node.rgt > children.rgt and node.id = :id;", nativeQuery = true)
+                   "where node.lft < children.lft and node.rgt > children.rgt and node.id = :id", nativeQuery = true)
     List<ReadNode> findDescendants(Long id);
 
     @Query(value = "select parent.* from nested_set as node, nested_set as parent\n" +
-                   "where node.lft > parent.lft and node.rgt < parent.rgt and node.id = :id;", nativeQuery = true)
+                   "where node.lft > parent.lft and node.rgt < parent.rgt and node.id = :id", nativeQuery = true)
     List<ReadNode> findAncestors(Long id);
 }
